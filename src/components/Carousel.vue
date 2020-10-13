8355817b20539dfd9df93a6bc656041d45145e2a
Helom Zive <helomzive@gmail.com>
2020-10-13 11:26:01 -0300

    agora sim foi todos os arquivos

---
+++ src/components/Carousel.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="carousel">
+        <slot></slot>
+        <button class="next" @click="next">Next</button>
+        <button class="prev" @click="prev">Prev</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        next() {
+            this.$emit('next');
+        },
+        prev() {
+            this.$emit('prev');
+        }
+    }
+}
+</script>
+
+<style>
+    .carousel{
+        position: relative;
+    }
+    button{
+        position: absolute;
+        height: 40px;
+        width: 50px;
+        top: calc(50% - 20px);
+        background-color: rgba(0, 0, 0, 0.6);
+        border: none;
+        color: aliceblue;
+    }
+    button:focus, button:hover{
+        outline: none;
+        cursor: pointer;
+    }
+    .next{
+        right: 0;
+        border-radius: 10px 0px 0px 10px ;
+    }
+    .prev{
+        left: 0;
+        border-radius: 0px 10px 10px 0px ;
+    }
+</style>
---
+++ src/components/CarouselSlide.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="visibleSlide === index" class="carousel-slide">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        key: Number,
+        index: Number,
+        visibleSlide: Number
+    }
+}
+</script>
+
+<style>
+    
+</style>
