b25b1a042b1460364e70c870dcbe2c016193e9ce
Helom Zive <helomzive@gmail.com>
2020-10-13 11:24:32 -0300

    projeto pokedex finalizado por hora, depois eu volto aqui

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,81 @@
 <template>
-    <div id="app">
-        <div class="column is-one-third is-offset-one-third">
-            <div v-for="(poke, index) in pokemons" :key="index">
-            <Pokemon :name="poke.name" :url="poke.url"/>
-            </div>
-        </div>
+    <div id="app" class="body">
+        <Carousel
+            @next="next"
+            @prev="prev"
+        >
+            <CarouselSlide v-for="(poke, index) in pokemons" 
+                :key="index" 
+                :index="index"
+                :visibleSlide="visibleSlide"
+                >
+                <Pokemon :name="poke.name" :url="poke.url"/>
+            </CarouselSlide>
+        </Carousel>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 import Pokemon from './components/Pokemon';
-
+import Carousel from './components/Carousel';
+import CarouselSlide from './components/CarouselSlide';
 
 export default {
     name: 'App',
     data(){
         return{
-            pokemons: []
+            pokemons: [],
+            visibleSlide: 0,
         }
     },
     created: function(){
         axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
             .then(res=>{
-                //console.log("Pegou os pokemons da primeira geração");
+                //console.log(this.countSlides);
                 this.pokemons = res.data.results;
             });
     },
+    computed:{
+        slidesLen(){
+            return this.pokemons.length;
+        }
+    },
+    methods:{
+        next(){
+            if (this.visibleSlide >= this.slidesLen - 1) {
+                this.visibleSlide = 0;
+            }else{
+                this.visibleSlide ++;
+            }
+        },
+        prev(){
+            if (this.visibleSlide <= 0) {
+                this.visibleSlide = this.slidesLen - 1;
+            }else{
+                this.visibleSlide --;
+            }
+        }
+    },
     components:{
-        Pokemon
+        Pokemon,
+        Carousel,
+        CarouselSlide
     }
 }
 </script>
 
 <style>
-
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .body{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        width: 100%;
+    }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,32 +1,12 @@
 <template>
-<div id="pokeCard">
-    <div class="card">
-        <div class="card-image">
-            <figure class="image is-square">
-            <img :src="pokemon.icon" alt="Placeholder image">
-            </figure>
+    <div id="pokeCard" class="card">
+        <div class="content">
+            <h3>#{{this.pokemon.id}} - {{upperName}}</h3>
+            <span v-for="(types, index) in pokemon.types" :key="index"> {{types.type.name.toUpperCase()}} </span><br>
         </div>
-        <div class="card-content">
-            <div class="media">
-            <div class="media-left">
-                <figure class="image is-96x96">
-                <img :src="currentImg" alt="Placeholder image">
-                </figure>
-            </div>
-            <div class="media-content">
-                <p class="title is-4">{{upperName}}</p>
-                <p class="subtitle is-6">{{pokemon.type}}</p>
-            </div>
-            </div>
-
-            <div class="content">
-                <button class="button is-success is-outlined" @click="backToFront">Mudar icone</button>
-                <br>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
-        </div>
+        <img :src="pokemon.icon" :alt="name">
     </div>
-</div>
+<!--<h6 v-for="(type, index) in pokemon.types" :key="index">{{type.type.name}}</h6>-->
 </template>
 
 <script>
@@ -35,12 +15,11 @@ import axios from 'axios';
 export default {
     created: function(){
         axios.get(this.url).then(res =>{
-            this.pokemon.type = res.data.types[0].type.name;
-            this.pokemon.icon = res.data.sprites.other.dream_world.front_default;
-            this.pokemon.front = res.data.sprites.front_default;
-            this.pokemon.back = res.data.sprites.back_default;
-            this.currentImg = this.pokemon.front
-            console.log(res.data.types)
+            this.pokemon.types = res.data.types;
+            this.pokemon.icon = res.data.sprites.other.['official-artwork'].front_default;
+            this.pokemon.id = res.data.id;
+            this.pokemon.height = res.data.height;
+            //console.log(res.data.sprites.other)
         })
     },
     data(){
@@ -48,11 +27,33 @@ export default {
             isFront: true,
             currentImg: '',
             pokemon: {
-                type: '',
+                types: [],
                 icon: '',
                 front: '',
-                back: ''
-            }
+                back: '',
+                id: '',
+                height:''
+            },
+            typesColor: {
+                grass: "#9bcc50",
+                poison: "#b97fc9",
+                fire: "#fd7d24",
+                psychic: "#f366b9",
+                flying: ["#3dc7ef","#bdb9b8"],
+                ice: "#51c4e7",
+                water: "#4592c4",
+                ground: ["#f7de3f","#ab9842"],
+                rock: "#a38c21",
+                electric: "#eed535",
+                bug: "#729f3f",
+                normal: "#a4acaf",
+                fighting: "#d56723",
+                fairy: "#fdb9e9",
+                steel: "#9eb7b8",
+                ghost: "#7b62a3",
+                dragon: ["#53a4cf", "#f16e57"],
+                dark: "#707070"
+            },
         }
     },
     props:{
@@ -73,13 +74,75 @@ export default {
                 this.isFront = true;
                 this.currentImg = this.pokemon.front;
             }
-        }
+        },
+        
     }
 }
 </script>
 
 <style>
-    #pokeCard{
-        margin: 10px;
+    .card{
+        position: relative;
+        width: 500px;
+        height: 250px;
+        margin: 20px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        border-radius: 30px;
+        background: linear-gradient(135deg, #9bcc50, #b97fc9);
+    }
+    .card img{
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        height: 200px;
+        transition: 500ms;
+    }
+    .card:hover img{
+        height: 350px;
+        left: 80%;
+    }
+    .card .content{
+        position: relative;
+        width: 50%;
+        left: 20%;
+        padding: 20px 20px 20px 40px;
+        opacity: 0;
+        visibility: hidden;
+        transition: 500ms;
+    }
+    .card .content h3{
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 25px;
+        font-weight: bold;
+        color: #fff;
+        text-shadow: 2px 4px 9px #677;
+    }
+    .card .content span{
+        display: block;
+        margin: 5px 10px;
+        padding: 8px 5px;
+        border: 1px solid #000;
+        border-radius: 15px;
+        text-align: center;
+        font-weight: bold;
+    }
+    .card .content a{
+        position: relative;
+        display: inline-block;
+        color:  #111;
+        padding: 10px 20px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: bold;
+        border-radius: 10px;
+        background-color: #ccc;
+        text-decoration: none;
+    }
+    .card:hover .content{
+        left: 0;
+        opacity: 1;
+        visibility: visible;
     }
 </style>
